d261ad930c8ac65dc7feca9ed07bde3a60094742
Glitch (cedar-kiwi) <none>
2019-08-21 13:02:15 +0000

    Step 7 working but install button not showing in heading

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,7 +18,7 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v4';
+const CACHE_NAME = 'static-cache-v6';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 // CODELAB: Add list of files to cache here.
